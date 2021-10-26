cae6874a765cccda2400bac8b0e195aace11d250
Kaiwen Sun <skw_kevin@126.com>
2021-10-25 19:24:02 -0700

    create asg

--- src/main/java/com/myorg/HelloCdkStack.java
+++ src/main/java/com/myorg/HelloCdkStack.java
@@ -1,10 +1,37 @@
 package com.myorg;
 
 import software.amazon.awscdk.core.Construct;
+import software.amazon.awscdk.core.RemovalPolicy;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.services.autoscaling.AutoScalingGroup;
+import software.amazon.awscdk.services.codedeploy.ServerApplication;
+import software.amazon.awscdk.services.codedeploy.ServerDeploymentGroup;
+import software.amazon.awscdk.services.ec2.ISecurityGroup;
+import software.amazon.awscdk.services.ec2.IVpc;
+import software.amazon.awscdk.services.ec2.InstanceClass;
+import software.amazon.awscdk.services.ec2.InstanceSize;
+import software.amazon.awscdk.services.ec2.InstanceType;
+import software.amazon.awscdk.services.ec2.MachineImage;
+import software.amazon.awscdk.services.ec2.SecurityGroup;
+import software.amazon.awscdk.services.ec2.UserData;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.ec2.VpcLookupOptions;
+import software.amazon.awscdk.services.iam.ManagedPolicy;
+import software.amazon.awscdk.services.iam.Role;
+import software.amazon.awscdk.services.iam.ServicePrincipal;
+import software.amazon.awscdk.services.s3.Bucket;
+
+import java.util.Collections;
+
 
 public class HelloCdkStack extends Stack {
+
+    private final static InstanceType instanceType = InstanceType.of(InstanceClass.BURSTABLE3, InstanceSize.NANO);
+    private final static String SECURITY_GROUP_ID = "sg-0061dfb13642c7d0a";  // ssh only, in us-east-1
+    private final static String REGION = "us-east-1";
+    private final static String KEY_PAIR_NAME = String.format("%s-ec2-keypair", REGION);
+
     public HelloCdkStack(final Construct scope, final String id) {
         this(scope, id, null);
     }
@@ -12,6 +39,54 @@ public class HelloCdkStack extends Stack {
     public HelloCdkStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
+        ISecurityGroup securityGroup = SecurityGroup.fromSecurityGroupId(this, "SecurityGroup", SECURITY_GROUP_ID);
+
+        Role ec2InstanceProfileRole = Role.Builder.create(this, "InstanceProfile")
+                .description("EC2 Instance Profile for CodeDeploy agent, managed by CDK")
+                .assumedBy(ServicePrincipal.Builder.create("ec2.amazonaws.com").build())
+                .managedPolicies(Collections.singletonList(
+                        ManagedPolicy.fromAwsManagedPolicyName("service-role/AmazonEC2RoleforAWSCodeDeploy")))
+                .build();
+
+        UserData userData = UserData.forLinux();
+
+        // install CodeDeploy agent
+        userData.addCommands(
+                "sudo yum update",
+                "sudo yum install ruby",
+                "sudo yum install wget",
+
+                "CODEDEPLOY_BIN=\"/opt/codedeploy-agent/bin/codedeploy-agent\"",
+                "$CODEDEPLOY_BIN stop",
+                "yum erase codedeploy-agent -y",
+
+                "cd /home/ec2-user",
+                String.format("wget https://aws-codedeploy-%s.s3.%s.amazonaws.com/latest/install", REGION, REGION),
+                "chmod +x ./install",
+                "sudo ./install auto"
+        );
+
+        IVpc vpc = Vpc.fromLookup(this, "VPC", VpcLookupOptions.builder()
+                .isDefault(true)
+                .build());
+
+        AutoScalingGroup asg = AutoScalingGroup.Builder.create(this, "ASG")
+                .keyName(KEY_PAIR_NAME)
+                .instanceType(instanceType)
+                .machineImage(MachineImage.latestAmazonLinux())
+                .securityGroup(securityGroup)
+                .role(ec2InstanceProfileRole)
+                .userData(userData)
+                .vpc(vpc)
+                .build();
+        ServerApplication application = ServerApplication.Builder.create(this, "Application").build();
+        ServerDeploymentGroup deploymentGroup = ServerDeploymentGroup.Builder
+                .create(this, "DeploymentGroup")
+                .autoScalingGroups(Collections.singletonList(asg))
+                .build();
+
+        Bucket.Builder.create(this, String.format("MyCodeDeployServerApplicationRevisionBucket-%s", REGION))
+                .versioned(true)
+                .build();
     }
 }
