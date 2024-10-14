b5dfaac4fb2e84ae8d1d15adf731cd7eeec51300
Kaiwen Sun <skw_kevin@126.com>
2024-10-14 13:08:11 -0700

    wrap codedeploy cli

--- bin/wait-deployment.sh
+++ bin/wait-deployment.sh
@@ -10,24 +10,28 @@ source bin/utils.sh
 
 stack_outputs=`aws cloudformation describe-stacks --stack-name $STACK_NAME --output json --query Stacks[0].Outputs`
 
+function codedeploy() {
+  aws deploy $@
+}
+
 CD_APPLICATION_NAME=`get_stack_output ApplicationName`
 CD_DEPLOYMENTGROUP_NAME=`get_stack_output DeploymentGroupName`
 
-deployment_id=`aws deploy list-deployments --application-name "${CD_APPLICATION_NAME}" --deployment-group-name "${CD_DEPLOYMENTGROUP_NAME}" --output text --query 'deployments[0]'`
+deployment_id=`codedeploy list-deployments --application-name "${CD_APPLICATION_NAME}" --deployment-group-name "${CD_DEPLOYMENTGROUP_NAME}" --output text --query 'deployments[0]'`
 
-aws deploy wait deployment-successful --deployment-id "${deployment_id}" || true
+codedeploy wait deployment-successful --deployment-id "${deployment_id}" || true
 
-deployment_info=`aws deploy get-deployment --deployment-id "${deployment_id}"`
+deployment_info=`codedeploy get-deployment --deployment-id "${deployment_id}"`
 
 # print out deployment info
 jq '.' <<< "$deployment_info"
 
-deployment_target_id=`aws deploy list-deployment-targets --deployment-id $deployment_id --output text --query 'targetIds[0]'`
+deployment_target_id=`codedeploy list-deployment-targets --deployment-id $deployment_id --output text --query 'targetIds[0]'`
 
 if [[ "${deployment_target_id}" == None ]]; then
   echo "No deployment targets found"
 else
-  aws deploy get-deployment-target --deployment-id $deployment_id --target-id $deployment_target_id
+  codedeploy get-deployment-target --deployment-id $deployment_id --target-id $deployment_target_id
 fi
 
 popd > /dev/null
