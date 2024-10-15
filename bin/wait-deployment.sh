361ca10b3a24764ade0be0614852915714495d69
Kaiwen Sun <skw_kevin@126.com>
2024-10-14 20:25:36 -0700

    print out deployment id and status

--- bin/wait-deployment.sh
+++ bin/wait-deployment.sh
@@ -19,12 +19,15 @@ CD_DEPLOYMENTGROUP_NAME=`get_stack_output DeploymentGroupName`
 
 deployment_id=`codedeploy list-deployments --application-name "${CD_APPLICATION_NAME}" --deployment-group-name "${CD_DEPLOYMENTGROUP_NAME}" --output text --query 'deployments[0]'`
 
+echo Waiting for deployment $deployment_id
+
 codedeploy wait deployment-successful --deployment-id "${deployment_id}" || true
 
 deployment_info=`codedeploy get-deployment --deployment-id "${deployment_id}"`
 
 # print out deployment info
 jq '.' <<< "$deployment_info"
+status=`jq -r '.deploymentInfo.status' <<< "${deployment_info}"`
 
 deployment_target_id=`codedeploy list-deployment-targets --deployment-id $deployment_id --output text --query 'targetIds[0]'`
 
@@ -33,5 +36,7 @@ if [[ "${deployment_target_id}" == None ]]; then
 else
   codedeploy get-deployment-target --deployment-id $deployment_id --target-id $deployment_target_id
 fi
+
+echo "Deployment $deployment_id $status"
 
 popd > /dev/null
